9d21b133c4a4ed6bdf7d637153948ba04702fcc7
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-08-18 13:49:14 +0300

    Aos once true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,5 +70,9 @@ export default defineNuxtConfig({
 	image: {
 		provider: "twicpics",
 	},
+	aos: {
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+	},
 	modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', "@nuxt/image", "nuxt-aos"]
 })
