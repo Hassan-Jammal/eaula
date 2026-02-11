6d893f110ba7b6f17f5defbb7e32ed264fa859ce
Hassan Al Jammal <hassan-jammal@hotmail.com>
2026-02-11 09:41:37 +0200

    .

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -238,7 +238,7 @@
                     <div class="w-1/2 pr-4 md:pr-12 py-12 border-r-[0.5px] border-white">
                         <p class="text-base" data-aos="fade-up">Bottles Eliminated <br /> (500mL)</p>
                         <AnimatedCounter
-                            :end-val="1841000"
+                            :end-val="2252300"
                             :duration="3"
                             :decimal-places="0"
                             suffix="+"
@@ -249,7 +249,7 @@
                     <div class="w-1/2 pl-4 md:pl-12 py-12 border-l-[0.5px] border-white">
                         <p class="text-base" data-aos="fade-up">CO2 Emissions <br /> Reduced</p>
                         <AnimatedCounter
-                            :end-val="151000"
+                            :end-val="185000"
                             :duration="3"
                             :decimal-places="0"
                             suffix=" kg+"
