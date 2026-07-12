d70948646b4b31cda4a3ca8aec8c6fe3b6418772
Hassan Jammal <hassan.aljammal@montyholding.com>
2026-07-12 14:50:32 +0300

    .

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -233,11 +233,11 @@
                 <div class="flex justify-between">
                     <div class="w-1/2 pr-4 md:pr-12 py-12 border-r-[0.5px] border-white">
                         <p class="text-base" data-aos="fade-up">Bottles Eliminated <br /> (500mL)</p>
-                        <AnimatedCounter :end-val="3847400" :duration="3" :decimal-places="0" suffix="+" wrapper-class="text-3xl md:text-7xl mt-4" data-aos="fade-up" />
+                        <AnimatedCounter :end-val="4331240" :duration="3" :decimal-places="0" suffix="+" wrapper-class="text-3xl md:text-7xl mt-4" data-aos="fade-up" />
                     </div>
                     <div class="w-1/2 pl-4 md:pl-12 py-12 border-l-[0.5px] border-white">
                         <p class="text-base" data-aos="fade-up">CO2 Emissions <br /> Reduced</p>
-                        <AnimatedCounter :end-val="315700" :duration="3" :decimal-places="0" suffix=" kg+" wrapper-class="text-3xl md:text-7xl mt-4" data-aos="fade-up" />
+                        <AnimatedCounter :end-val="355850" :duration="3" :decimal-places="0" suffix=" kg+" wrapper-class="text-3xl md:text-7xl mt-4" data-aos="fade-up" />
                     </div>
                 </div>
             </div>
